f74b4fc93397f707ee5d2c0837b391647755db5f
Łukasz Gołko <lukaszgolko26@gmail.com>
2026-06-04 20:08:04 +0200

    inertia.php typo

--- config/inertia.php
+++ config/inertia.php
@@ -61,7 +61,7 @@ return [
 
     'pages' => [
         'ensure_pages_exist' => false,
-        'paths' => [resource_path('js/Pages')],
+        'paths' => [resource_path('js/pages')],
         'extensions' => ['js', 'jsx', 'svelte', 'ts', 'tsx', 'vue'],
     ],
 
